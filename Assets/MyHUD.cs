f280c926b2a6c31031743bf3a6a5d8500bbad73c
DESKJULES\Admin <lordstok@hotmail.com>
2016-11-29 14:41:17 +1100

    lecture 286 - end state

---
+++ Assets/MyHUD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyHUD : MonoBehaviour {
+
+	private NetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+		networkManager = GetComponent<NetworkManager>();
+	}
+
+	public void MyStartHost (){
+		Debug.Log ("Starting Network Host at " + Time.timeSinceLevelLoad);
+		networkManager.StartHost();
+	}
+
+	void OnStartHost() {
+		Debug.Log ("Network Host started at " + Time.timeSinceLevelLoad);
+	}
+
+}
