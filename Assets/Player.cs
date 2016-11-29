9226a5f255dc0acdfa476974863dfe74120e6afa
DESKJULES\Admin <lordstok@hotmail.com>
2016-11-29 21:35:13 +1100

    lecture 290 - add crossplatformcontrol challenge - test on network client

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,17 +4,32 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 	
-	// Use this for initialization
-	void Start () {
+	public float speed = 10.0f;
+	public float rotationSpeed = 100.0f;
 		
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		Debug.Log ("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
 		Debug.Log ("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
-		Debug.Log ("Fire 1: " + CrossPlatformInputManager.GetButtonDown("Fire 1"));
+	
+		float translation = CrossPlatformInputManager.GetAxis("Vertical") * speed;
+		float rotation = CrossPlatformInputManager.GetAxis("Horizontal") * rotationSpeed;
 		
-		//	if (CrossPlatformInputManager.GetButtonDown
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;				
+		
+		transform.Translate (0,0,translation);
+		transform.Rotate(0,rotation,0);
+		
+//		if (CrossPlatformInputManager.GetAxis("Horizontal")) {
+//			this.transform.position += Vector3.left * speed * Time.deltaTime;
+//		}
+//		else if (CrossPlatformInputManager.GetButtonDown("Right")) {
+//			this.transform.position += Vector3.right * speed * Time.deltaTime;
+//		}
+
 	}
+	
+	
+	
 }
